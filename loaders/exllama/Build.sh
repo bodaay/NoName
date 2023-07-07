f32cbd6aee493bc5bd4a45339c1ed324eda2f035
Khalefa <khalefa@alahmad.org>
2023-07-07 18:07:38 +0300

    fixed build script

--- loaders/exllama/Build.sh
+++ loaders/exllama/Build.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 CUDAVERSON="11.8"
-CMAKE_PREFIX_PATH=${PWD}/../../libtorch/src/
-echo $CMAKE_PREFIX_PATH
 rm -rf build
 mkdir build
 cd build
 
 
-cmake -DCUDA_BIN_PATH=/usr/local/cuda-$CUDAVERSON/bin/ -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-$CUDAVERSON/ -DCMAKE_PREFIX_PATH=/home/khalefa/projects/NoName/libtorch/src/ ..
+cmake -DCUDA_BIN_PATH=/usr/local/cuda-$CUDAVERSON/bin/ -DCUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-$CUDAVERSON/ -DCMAKE_PREFIX_PATH=../../libtorch/src/ ..
 cmake --build . --config Release && mv libexllama.so ../../lib/
 
 #cleanup
