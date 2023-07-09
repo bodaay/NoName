d9ed3f96a2db5d754822fa1b261e13fe2c0cf4c6
Khalefa <khalefa@alahmad.org>
2023-07-09 22:11:08 +0300

    protoc, grpc install script perfect now

--- loaders/bridge/InstallProtocAndGRPC.sh
+++ loaders/bridge/InstallProtocAndGRPC.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PROTOC_VERSION="23.4"
+PROTOBUF_VERSION="23.4"
 GRPC_VERSION="v1.56.0"
 # Update system
 sudo apt-get update
@@ -15,16 +15,42 @@ do
 done
 
 # Download protobuf if not exist
-# https://github.com/protocolbuffers/protobuf/releases/download/v23.4/protoc-23.4-linux-x86_64.zip
-if [ ! -f "/usr/local/bin/protoc" ];
-then                            
-    curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOC_VERSION/protoc-$PROTOC_VERSION-linux-x86_64.zip
-    unzip protoc-$PROTOC_VERSION-linux-x86_64.zip -d protoc3
-    sudo mv protoc3/bin/* /usr/local/bin/
-    sudo mv protoc3/include/* /usr/local/include/
-    sudo ldconfig
-    rm -rf protoc3 protoc-$PROTOC_VERSION-linux-x86_64.zip
-fi
+# Download, compile and install protobuf
+if [ -f "/usr/local/bin/protoc" ]; then                                                                                                                                                                                                                                               
+    read -p "/usr/local/bin/protoc exists. Do you want to reinstall? (y/n) " -n 1 -r                                                                                                                                                                                                  
+    echo                                                                                                                                                                                                                                                                              
+    if [[ $REPLY =~ ^[Yy]$ ]]                                                                                                                                                                                                                                                         
+    then                                                                                                                                                                                                                                                                              
+        # https://github.com/protocolbuffers/protobuf/releases/download/v23.4/protobuf-23.4.tar.gz                                                                                                                                                                                    
+        curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOBUF_VERSION/protobuf-$PROTOBUF_VERSION.tar.gz                                                                                                                                                   
+        tar xzf protobuf-$PROTOBUF_VERSION.tar.gz                                                                                                                                                                                                                                     
+        cd protobuf-$PROTOBUF_VERSION/third_party                                                                                                                                                                                                                                     
+        rm -rf abseil-cpp                                                                                                                                                                                                                                                             
+        git clone https://github.com/abseil/abseil-cpp                                                                                                                                                                                                                                
+        cd ../                                                                                                                                                                                                                                                                        
+        cmake -Dprotobuf_BUILD_TESTS=OFF .                                                                                                                                                                                                                                            
+        cmake --build . --parallel 10                                                                                                                                                                                                                                                 
+        sudo cmake --install .                                                                                                                                                                                                                                                        
+        sudo ldconfig                                                                                                                                                                                                                                                                 
+        cd ..                                                                                                                                                                                                                                                                         
+        rm -rf protobuf-$PROTOBUF_VERSION protobuf-$PROTOBUF_VERSION.tar.gz                                                                                                                                                                                                           
+    fi                                                                                                                                                                                                                                                                                
+else                                                                                                                                                                                                                                                                                  
+    # https://github.com/protocolbuffers/protobuf/releases/download/v23.4/protobuf-23.4.tar.gz                                                                                                                                                                                        
+    curl -OL https://github.com/protocolbuffers/protobuf/releases/download/v$PROTOBUF_VERSION/protobuf-$PROTOBUF_VERSION.tar.gz                                                                                                                                                       
+    tar xzf protobuf-$PROTOBUF_VERSION.tar.gz                                                                                                                                                                                                                                         
+    cd protobuf-$PROTOBUF_VERSION/third_party                                                                                                                                                                                                                                         
+    rm -rf abseil-cpp                                                                                                                                                                                                                                                                 
+    git clone https://github.com/abseil/abseil-cpp                                                                                                                                                                                                                                    
+    cd ../                                                                                                                                                                                                                                                                            
+    cmake -Dprotobuf_BUILD_TESTS=OFF .                                                                                                                                                                                                                                                
+    cmake --build . --parallel 10                                                                                                                                                                                                                                                     
+    sudo cmake --install .                                                                                                                                                                                                                                                            
+    sudo ldconfig                                                                                                                                                                                                                                                                     
+    cd ..                                                                                                                                                                                                                                                                             
+    rm -rf protobuf-$PROTOBUF_VERSION protobuf-$PROTOBUF_VERSION.tar.gz                                                                                                                                                                                                               
+fi   
+
 
 # Clone gRPC if not exist
 
