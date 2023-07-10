26367b933cfaf355cc3fa104df14ead0b42cbb37
Khalefa <khalefa@alahmad.org>
2023-07-10 22:46:43 +0300

    k

--- loaders/bridge/Build.sh
+++ loaders/bridge/Build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export PATH="/usr/local/cuda-11.8/bin:$PATH"
-export LD_LIBRARY_PATH="/usr/local/cuda-11.8/lib64:$LD_LIBRARY_PATH"
+# PATH="/usr/local/cuda-11.8/bin:$PATH"
+# LD_LIBRARY_PATH="/usr/local/cuda-11.8/lib64:$LD_LIBRARY_PATH"
 CUDAVERSON="11.8"
 # rm -rf build
 mkdir -p build
